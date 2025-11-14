3398de5348af36218e43f396b5b71c9d6ef8dfe1
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-11-14 13:13:04 +0900

    feat: add time recording

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use rubiks_cube::{self, Cube, Operation};
 
 fn operation_vec_to_string(operations: Vec<Operation>) -> String {
@@ -9,14 +11,33 @@ fn operation_vec_to_string(operations: Vec<Operation>) -> String {
 }
 
 fn main() {
-    let (cube, applied) = Cube::random(5);
-    println!("Applied rotation: {}", operation_vec_to_string(applied));
+    let num_trials = 30;
+    let num_shuffles = 6;
+    let search_depth = 6;
+    let mut total_time = Duration::ZERO;
 
-    let result = rubiks_cube::solve(cube, 5);
+    for i in 0..num_trials {
+        println!("Trial #{}/{num_trials}", i + 1);
 
-    if let Some(result) = result {
-        println!("Found: {}", operation_vec_to_string(result));
-    } else {
-        println!("Not found")
+        let (cube, applied) = Cube::random(num_shuffles);
+        println!("Applied rotation: {}", operation_vec_to_string(applied));
+
+        let start_time = Instant::now();
+        let result = rubiks_cube::solve(cube, search_depth);
+        let process_time = start_time.elapsed();
+
+        if let Some(result) = result {
+            print!("Found: {}", operation_vec_to_string(result));
+        } else {
+            print!("Not found")
+        }
+        println!(" ({:.2} secs)", process_time.as_secs_f64());
+
+        total_time += process_time
     }
+
+    println!(
+        "Average time: {:.2} secs",
+        total_time.as_secs_f64() / (num_trials as f64)
+    );
 }
